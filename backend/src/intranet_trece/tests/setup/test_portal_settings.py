4d25d2950d705f05297bbcd59abfc2aa2b3d5dd1
ronyerisson <ronyerisson@tre-ce.jus.br>
2024-02-20 13:58:08 -0300

    Exercício 13 - Testa o título do portal, Timezone, Sitemap e E-mail

--- backend/src/intranet_trece/tests/setup/test_portal_settings.py
+++ backend/src/intranet_trece/tests/setup/test_portal_settings.py
@@ -1,8 +1,24 @@
 from plone import api
 
+
 class TestPortalProperties:
 
-    def test_portal_title(self,portal):
-        expected="Intranet TRE-CE"
-        value= api.portal.get_registry_record("plone.site_title")
-        assert value==expected, f"Value '{}' is not equal '{expected}'"
+    def test_portal_title(self, portal):
+        expected = "Nova Intranet TRE-CE"
+        value = api.portal.get_registry_record("plone.site_title")
+        assert value == expected, f"Value '{value}' is not equal '{expected}'"
+
+    def test_portal_timezone(self, portal):
+        expected = "America/Fortaleza"
+        value = api.portal.get_registry_record("plone.portal_timezone")
+        assert value == expected, f"Value '{value}' is not equal '{expected}'"
+
+    def test_portal_sitemap(self, portal):
+        expected = True
+        value = api.portal.get_registry_record("plone.enable_sitemap")
+        assert value is expected, f"Value '{value}' is not '{expected}'"
+
+    def test_portal_email_from_name(self, portal):
+        expected = "Nova Intranet TRE-CE"
+        value = api.portal.get_registry_record("plone.email_from_name")
+        assert value == expected, f"Value '{value}' is not equal '{expected}'"
